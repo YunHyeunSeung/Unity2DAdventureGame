b0a8ca97d088ef60e841b79c0c3672df3e009100
Myoungcholho <110521729+Myoungcholho@users.noreply.github.com>
2023-04-19 00:38:39 +0900

    Update helloWorld.cs

--- Assets/Script/helloWorld.cs
+++ Assets/Script/helloWorld.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class helloWorld : MonoBehaviour
 {
-<<<<<<< HEAD
     public Vector3 postion;
-=======
-    public Transform transform;
->>>>>>> 2c067410f6d677ca542ece3d4a733d0ceb6bf6af
+
     // Start is called before the first frame update
     void Start()
     {
